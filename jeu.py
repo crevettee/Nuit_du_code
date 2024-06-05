4f991ce42bfc44874d18c5f0a20d20ab31b5481a
MouleFarcie <quent1richard@yahoo.com>
2024-06-05 16:01:41 +0200

    modified:   jeu.py

--- jeu.py
+++ jeu.py
@@ -3,9 +3,10 @@ pyxel.init(128, 128, title="NDC 2024")
 
 def update():
     if pyxel.btn(pyxel.KEY_SPACE):
-        print("connard")
+        print("connardouhiughiuh")
 
 def draw():
     pyxel.rect(0,0,3,3,9)
+
 
 pyxel.run(update,draw)
